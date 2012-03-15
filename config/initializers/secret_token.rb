41d6765338e88abbbb432a68062e6218924516fa
Carbon Crank <johnmforsyth@yahoo.co.uk>
2012-03-15 14:31:09 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo3::Application.config.secret_token = '1ac7a7b7b3e60606caaedb9f9e6cfc9bfe5c768fa06bb4933046bdb3742a739d11ea687a942f4c952a21a8f09cd73633cbca6ca4607deff8444c5ae24c6a043f'
